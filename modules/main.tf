86d6ddfaa2826dff93b7029a7215ca2bf2d2c6b8
shashidharavuta291@gmail.com <avutashashi291>
2024-09-30 10:09:02 +0530

    change the code

--- modules/main.tf
+++ modules/main.tf
@@ -1,8 +1,12 @@
+provider "aws" {
+  region = "ap-southeast-1" # Change to your preferred region
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-08718895af4dfa033" 
   instance_type = "t2.micro"
   subnet_id = "subnet-045941d0dea5b226f"
-   
+
   tags = {
     Name = "TerraformExampleInstance"
   }
--- modules/variable.tf
+++ modules/variable.tf
@@ -1,3 +1,8 @@
+variable "region" {
+  description = "The AWS region to deploy the instance in."
+  default     = "ap-southeast-1"
+}
+
 variable "ami" {
   description = "The AMI ID for the instance."
   default     = "ami-08718895af4dfa033" # Change to a valid AMI ID for your region
@@ -6,4 +11,8 @@ variable "ami" {
 variable "instance_type" {
   description = "The type of instance."
   default     = "t2.micro"
+}
+
+variable "subnet_id" {
+  default = "subnet-045941d0dea5b226f"
 }
