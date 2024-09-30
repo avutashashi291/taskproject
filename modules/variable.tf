74943c180d23cd0b7f82aca19b18ad08023b5345
shashidharavuta291@gmail.com <avutashashi291>
2024-09-30 10:01:52 +0530

    change the code

--- modules/variable.tf
+++ modules/variable.tf
@@ -1,6 +1,6 @@
 variable "ami" {
   description = "The AMI ID for the instance."
-  default     = "ami-0c55b159cbfafe01e" # Change to a valid AMI ID for your region
+  default     = "ami-08718895af4dfa033" # Change to a valid AMI ID for your region
 }
 
 variable "instance_type" {
